80e28d24b651083e3a6c73b4aad2a3f96188badf
booking1on1its <107371643+booking1on1its@users.noreply.github.com>
2022-06-13 14:10:15 +0700

    Update simpan.php

--- simpan.php
+++ simpan.php
@@ -2,13 +2,13 @@
 
 include "koneksi.php";
 
-if (isset($POST['title'])) {
+if (isset($_POST['title'])) {
     
-    $tittle = $_POST['title'];
+    $title = $_POST['title'];
     $start = $_POST['start'];
     $end = $_POST['end'];
 
-    mysqli_query($koneksi, "INSERT into events VALUES ('', '$tittle', '$start', '$end') ");
+    mysqli_query($koneksi, "INSERT into events VALUES ('', '$title', '$start', '$end') ");
     
 
 }
